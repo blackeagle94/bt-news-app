ddad6aa10649bd195e5418c9e44fab769466c792
blackeagle94 <blackeagle4894@gmail.com>
2021-02-25 00:28:11 +0200

    completed

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Magazine from './Magazine';
+
+function App() {
+	const APP_KEY = 'abc8d87d7c574af69335c90025f2638a';
+
+	const [ news, setNews ] = useState([]);
+
+	const [ search, setSearch ] = useState('');
+
+	const [ query, setQuery ] = useState('fun');
+
+	useEffect(
+		() => {
+			getNews();
+		},
+		[ query ]
+	);
+
+	const getNews = async () => {
+		const response = await fetch(
+			`https://newsapi.org/v2/everything?q=${query}&from=2021-02-24&sortBy=popularity&pageSize=10&apiKey=${APP_KEY}`
+		);
+		const data = await response.json();
+
+		setNews(data.articles);
+		console.log(data.articles);
+	};
+	const updateSearch = (e) => {
+		setSearch(e.target.value);
+		console.log(search);
+	};
+
+	const getSearch = (e) => {
+		
+		e.preventDefault();
+    if (search.trim() == '') return false
+		setQuery(search);
+		setSearch('');
+	};
+
+	return (
+		<div className="App">
+			<Header getSearch={getSearch} updateSearch={updateSearch} search={search}/>
+      <div className="h1">
+        <h1>BT React Code Test - by Mursel AYSAN - 25/02/21</h1>
+      </div>
+			{/* <div className="searchBar">
+      <form onSubmit={getSearch} className="search-form">
+				<input className="search-bar" type="text" value={search} onChange={updateSearch} />
+				<button className="search-button" type="submit">
+					Search
+				</button>
+			</form>
+      </div> */}
+      <div className="row">
+      {news && news.map((item, index) => (
+				<Magazine
+					key={index}
+					title={item.title}
+					author={item.author}
+					description={item.description}
+					urlToImage={item.urlToImage}
+					url={item.url}
+				/>
+			))}
+      </div>
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Magazine.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Magazine.css';
+
+export default function Magazine({ title, author, description, url, urlToImage }) {
+	return (
+		<div className="news">
+			<img className="news-image" src={urlToImage} alt="" />
+			<h1 className="news-title">{title}</h1>
+			<p>{description}</p>
+			<p>Author : <span style={{'color': '#333333'}}>{author}</span></p>
+			<button className="read-btn">
+				<a href={url}>Read More</a>
+			</button>
+		</div>
+	);
+}
